234fb90002ff6a0a2407023857fab438c5b5f32a
Thien Nguyen <s3426515@rmit.edu.vn>
2021-07-28 16:14:55 +1000

    Add background helper for files and directories

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,20 @@
 const fs = require('fs');
+const { dialog } = require('electron');
 
 export default {
+    readData : (filePath) => {
+        try {
+            return JSON.parse(fs.readFileSync(filePath));
+        } catch(e) { console.log(e); }
+    },
+    writeData : (args) => {
+        try {
+            fs.writeFileSync(args.path, JSON.stringify(args.data));
+        } catch(e) { console.log(e); }
+    },
+    getDir : async (multiple = false) => {
+        return await dialog.showOpenDialog({
+            properties: multiple ? ['openDirectory', 'multiSelections'] : ['openDirectory']
+        });
+    },
 }
