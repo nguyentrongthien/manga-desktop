a005bf0b908171ab608ff3dc2c300326d835c0ab
Thien Nguyen <s3426515@rmit.edu.vn>
2021-08-16 12:05:16 +1000

    Add custom scrollbar style globally

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,6 @@ export default {
     },
     created () {
         window.ipcRenderer.receive('from-main', (payload) => {
-            if(payload.passThrough) console.log(payload);
             if(payload.passThrough) this.$store.dispatch(payload.passThrough.flag, payload);
         })
     },
@@ -55,3 +54,42 @@ export default {
     }
 };
 </script>
+
+<style>
+    *::placeholder {
+        color: #b2aba1;
+    }
+    *::-webkit-scrollbar {
+        background: transparent;
+        width: 7px;
+    }
+    *::-webkit-scrollbar:hover {
+        /*background: linear-gradient(0deg, black 5%, #4e4e4e 50%, black 95%);*/
+        /*background: linear-gradient(90deg, black, #3c3c3c);*/
+        width: 7px;
+    }
+    *::-webkit-scrollbar-thumb {
+        border-radius: 5px;
+        background: radial-gradient(#939393 5%, #ffffff00 40%);
+    }
+    *::-webkit-scrollbar-thumb:hover {
+        background: radial-gradient(white 5%, black 80%);
+        border: 1px solid #c4c4c4;
+    }
+    *::-webkit-scrollbar-thumb:active {
+        background-color: #484e51;
+    }
+    *::-webkit-scrollbar-corner {
+        background-color: #181a1b;
+    }
+    *::selection {
+        background-color: #383838 !important;
+        color: #e8e6e3 !important;
+    }
+    * input:-webkit-autofill,
+    * textarea:-webkit-autofill,
+    * select:-webkit-autofill {
+        background-color: #555b00 !important;
+        color: #e8e6e3 !important;
+    }
+</style>
