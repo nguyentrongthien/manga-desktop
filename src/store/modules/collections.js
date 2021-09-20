27ee49de08c613f41dd78d436c0e2fde9b82ee20
Thien Nguyen <s3426515@rmit.edu.vn>
2021-09-20 23:57:46 +1000

    Add new Collections module for Vuex

---
+++ src/store/modules/collections.js
@@ -0,0 +1,19 @@
+const state = {
+    collections: []
+};
+const getters = {
+    collections: state => state.collections
+};
+const mutations = {
+
+};
+const actions = {
+
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
